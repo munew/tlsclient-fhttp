bbdda73937d391f331b0217a8b6dc1a0fa5f3187
munew <mune3@proton.me>
2026-05-03 20:12:09 +0200

    fix: disable cookie header split var not working

--- http2/transport.go
+++ http2/transport.go
@@ -1841,7 +1841,7 @@ func (cc *ClientConn) encodeHeaders(req *http.Request, addGzipHeader bool, trail
 				// fields. We have already checked if any
 				// are error-worthy so just ignore the rest.
 				continue
-			} else if strings.EqualFold(kv.Key, "cookie") {
+			} else if strings.EqualFold(kv.Key, "cookie") && !req.DisableCookieHeaderSplit {
 				// Per 8.1.2.5 To allow for better compression efficiency, the
 				// Cookie header field MAY be split into separate header fields,
 				// each with one or more cookie-pairs.
